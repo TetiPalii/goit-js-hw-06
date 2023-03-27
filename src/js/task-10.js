62e2a4e2ef4ec6536c7364c2d5e948396897df55
TetiPalii <TetianaPalii9491@gmail.com>
2023-03-27 12:48:52 +0200

    доопрацювання

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -8,7 +8,7 @@ const createBtn = document.querySelector('[data-create]');
 const destroyBtn = document.querySelector('[data-destroy]');
 const boxesContainerRef = document.querySelector('#boxes');
 const inputRef = document.querySelector('[type="number"]');
-const amount = inputRef.value;
+
 
 
 
@@ -16,23 +16,31 @@ createBtn.addEventListener('click', onCreateBtnClick);
 destroyBtn.addEventListener('click', onDestroyBtnClick);
 
 
+
 function onCreateBtnClick() {
-  createBoxes(amount)
+createBoxes(inputRef.value)
+}
+  
+function onDestroyBtnClick() {
+ 
  }
-function onDestroyBtnClick() { }
 
 function createBoxes(amount) {
-  return(amount)
-  
-  const box = document.createElement("div")
-  box.style.backgroundColor  = "red";
-  box.style.width = "30px";
-  box.style.height = '30px';
-  boxesContainerRef.append(box)
-  // console.log(boxesContainerRef.children.length)
 
+  for (let i = 0; i < Number(amount); i += 1){
+    const box = `<div class = "box"></div>`;
+    const allBoxes = allBoxes + box;
+    console.log(allBoxes)
+  }
+   
+  boxesContainerRef.insertAdjacentHTML("afterbegin", allBoxes.join(" "))
+  
+ 
+  return boxesContainerRef
 }
 
-function destroyBoxes() {
-  
-}
+
+
+
+
+
